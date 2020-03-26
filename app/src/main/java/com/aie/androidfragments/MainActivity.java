dbbd46d5c33ef28d6d0fa32905a4261c6520a35b
shivanthi-fernando <58467116+shivanthi-fernando@users.noreply.github.com>
2020-03-26 20:10:05 +0530

    commit

---
+++ app/src/main/java/com/aie/androidfragments/MainActivity.java
@@ -0,0 +1,37 @@
+package com.aie.androidfragments;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeFragment(View view){
+        Fragment fragment;
+
+        if (view == findViewById(R.id.frag1_btn)){
+            fragment = new Fragment1();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fgmntDefault,fragment);
+            ft.commit();
+        }
+
+        if (view == findViewById(R.id.frag2_btn)){
+            fragment = new Fragment2();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fgmntDefault,fragment);
+            ft.commit();
+        }
+    }
+}
